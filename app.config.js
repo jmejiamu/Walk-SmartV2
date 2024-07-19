add07bd5851e61496453df9fc95f566e2cd867b5
jmejiamu <jmejiamu@gmail.com>
2024-07-19 12:55:44 -0700

    feat(storybook): setup story book

---
+++ app.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  expo: {
+    name: "smart-walk",
+    slug: "smart-walk",
+    version: "1.0.0",
+    extra: {
+      storybookEnabled: process.env.EXPO_PUBLIC_STORYBOOK_ENABLED,
+    },
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    userInterfaceStyle: "light",
+    splash: {
+      image: "./assets/splash.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff",
+    },
+    assetBundlePatterns: ["**/*"],
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: "com.anonymous.smartwalk",
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#ffffff",
+      },
+      package: "com.anonymous.smartwalk",
+    },
+    web: {
+      favicon: "./assets/favicon.png",
+    },
+  },
+};
