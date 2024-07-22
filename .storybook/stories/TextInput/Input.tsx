8c47df0f5ec526da4648cc0524b2a27e5263821b
jmejiamu <jmejiamu@gmail.com>
2024-07-22 15:48:51 -0700

    feat(txtinput): TextInput Storybook

---
+++ .storybook/stories/TextInput/Input.tsx
@@ -0,0 +1,53 @@
+import {
+  View,
+  TextInput,
+  TextInputProps,
+  TextStyle,
+  StyleProp,
+} from "react-native";
+import React from "react";
+import { Spacing, spacing, Pallet, pallet } from "../../../src/themes";
+
+export type SpacingOption = Spacing;
+export type ColorPallet = Pallet;
+
+export type InputProps = {
+  placeholder: string;
+  size: SpacingOption;
+  bgColor: ColorPallet;
+  textInputProps?: TextInputProps;
+  placeholderTextColor?: ColorPallet;
+  inputTxtColor?: ColorPallet;
+  inputStyle: StyleProp<TextStyle>;
+};
+
+export const Input = (props: InputProps) => {
+  const {
+    placeholder = "Default Placeholder",
+    size = "sm",
+    bgColor = "primary_80",
+    textInputProps,
+    placeholderTextColor = "neutral_50",
+    inputTxtColor = "neutral_0",
+    inputStyle,
+  } = props;
+
+  let container: StyleProp<TextStyle> = {
+    padding: spacing[size],
+    borderRadius: spacing[size] * 2,
+    backgroundColor: pallet[bgColor],
+    fontSize: spacing[size],
+    color: pallet[inputTxtColor],
+  };
+
+  return (
+    <View>
+      <TextInput
+        style={[container, inputStyle]}
+        placeholder={placeholder}
+        placeholderTextColor={pallet[placeholderTextColor]}
+        {...textInputProps}
+      />
+    </View>
+  );
+};
