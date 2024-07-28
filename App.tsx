401d539aac41167732eb590f7757f723d864df4a
jmejiamu <jmejiamu@gmail.com>
2024-07-28 16:33:58 -0700

    feat(reduxtk-setup): initial RTK setup

--- App.tsx
+++ App.tsx
@@ -1,13 +1,16 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import Constants from "expo-constants";
-
+import { Provider as ReduxProvider } from "react-redux";
+import { store } from "./src/redux";
 const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ReduxProvider store={store}>
+      <View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </ReduxProvider>
   );
 };
 
---
+++ src/redux/store/store.ts
@@ -0,0 +1,8 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+
+const rootReducer = combineReducers({});
+export type RootState = ReturnType<typeof rootReducer>;
+
+export const store = configureStore({
+  reducer: rootReducer,
+});
